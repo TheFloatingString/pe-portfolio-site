88acc40912381eb921945a7b387d29da9f05a6c4
Laurence Liang <laurence.w.liang@gmail.com>
2025-07-05 19:31:44 -0400

    Update redeploy-site.sh

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,5 +1,5 @@
 git fetch && git reset origin/main --hard
 source .venv/bin/activate
 pip3 install -r requirements.txt # I'm having some issues here
-tmux new-session -d -s flask_session 'flask run'
+tmux new-session -d -s flask_session 'flask run -host "0.0.0.0"'
 tmux attach
