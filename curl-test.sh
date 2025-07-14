920cc3495999e1b37659fc8b08d066c9865e798f
TheFloatingString <laurence.w.liang@gmail.com>
2025-07-14 21:34:27 +0000

    feat: add mysql

---
+++ curl-test.sh
@@ -0,0 +1,2 @@
+curl -X POST http://localhost:5000/api/timeline_post -d 'name=wei&email=wei@mlh.com&content=test'
+curl -X GET http://localhost:5000/api/timeline_post -d 'name=wei&email=wei@mlh.com&content=test'
---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from pe-portfolio-site!")
+
+
+if __name__ == "__main__":
+    main()
